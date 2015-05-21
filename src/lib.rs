cef9f7b24895f2c35c244a855578eca0ebe904b8
jeron <snowtop105@gmail.com>
2015-05-21 17:25:41 -0500

    some structural changes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-pub mod zip {
-	extern "C" {
-		fn zip_open();
-	}
+extern {
+	#[allow(non_snake_case)]
+	#[no_mangle]
+	fn zip_open(path:*const i8, flags:i32, errorp:*mut i32) -> *mut i8;
+}
 
-	pub fn open() {
-		unsafe{ zip_open() }
-	}
+pub fn _lzip_open(path:*const i8, flags:i32, errorp:*mut i32) -> *mut i8 {
+	unsafe{ zip_open(path, flags, errorp) }
 }
 
 //Suport For C
